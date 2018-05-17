67798e76b5d30927e75ec94f34cef89375aa45f3
koanse <ant.kondaurov@gmail.com>
2018-05-18 01:47:57 +0300

    Initial commit.

---
+++ ProxyServerCs/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ProxyServerCs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proxyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startListenerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.proxyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // proxyToolStripMenuItem
+            // 
+            this.proxyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startListenerToolStripMenuItem});
+            this.proxyToolStripMenuItem.Name = "proxyToolStripMenuItem";
+            this.proxyToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.proxyToolStripMenuItem.Text = "Proxy";
+            // 
+            // startListenerToolStripMenuItem
+            // 
+            this.startListenerToolStripMenuItem.Name = "startListenerToolStripMenuItem";
+            this.startListenerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startListenerToolStripMenuItem.Text = "Start listener";
+            this.startListenerToolStripMenuItem.Click += new System.EventHandler(this.startListenerToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "125";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Proxy\'s port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Host name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "www.yandex.ru";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "US (bytes):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(92, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DS (bytes):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(92, 112);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(188, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 148);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Simple Proxy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proxyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startListenerToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+
+
+    }
+}
+
---
+++ ProxyServerCs/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ProxyServerCs
+{
+    public partial class Form1 : Form
+    {
+        delegate void RefreshFormDelegate();
+        Thread thread;
+        int upTraf, downTraf;
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+        public void RefreshStats()
+        {
+            textBox3.Text = upTraf.ToString();
+            textBox4.Text = downTraf.ToString();
+        }
+        private void TransferData(object arg)
+        {
+            ThreadProcParams tpp = arg as ThreadProcParams;
+            int port = tpp.port;
+            string host = tpp.host;
+            RefreshFormDelegate refresh = new RefreshFormDelegate(RefreshStats);
+            while (true)
+            {
+                TcpClient clientProxy = null;
+                TcpListener listener = null;
+                TcpClient clientReal = null;
+                NetworkStream nsProxy = null;
+                NetworkStream nsReal = null;
+                try
+                {
+                    clientProxy = new TcpClient(host, 80);
+                    listener = new TcpListener(IPAddress.Any, port);
+                    nsProxy = clientProxy.GetStream();
+                    listener.Start();
+                    clientReal = listener.AcceptTcpClient();
+                    nsReal = clientReal.GetStream();
+                    int bufSize = 1024;
+                    byte[] buf = new byte[bufSize];
+                    int charsReaded = bufSize;
+                    do
+                    {
+                        Thread.Sleep(100);
+                        charsReaded = nsReal.Read(buf, 0, bufSize);
+                        nsProxy.Write(buf, 0, charsReaded);
+                        upTraf += charsReaded;
+                    }
+                    while (nsReal.DataAvailable);
+                    charsReaded = bufSize;
+                    do
+                    {
+                        Thread.Sleep(100);
+                        charsReaded = nsProxy.Read(buf, 0, bufSize);
+                        nsReal.Write(buf, 0, charsReaded);
+                        downTraf += charsReaded;
+                        this.Invoke(refresh);
+                    }
+                    while (nsProxy.DataAvailable);
+                    listener.Stop();
+                    clientProxy.Close();
+                    clientReal.Close();
+                }
+                catch
+                {
+                    try
+                    {
+                        listener.Stop();
+                        nsProxy.Close();
+                        nsReal.Close();
+                        clientProxy.Close();
+                        clientReal.Close();
+                    }
+                    catch { }
+                }
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+            Close();
+        }
+        private void startListenerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int port;
+            string host;
+            try
+            {
+                port = int.Parse(textBox1.Text);
+                host = textBox2.Text;
+            }
+            catch
+            {
+                return;
+            }
+            ThreadProcParams tpp = new ThreadProcParams(port, host);
+            thread = new Thread(new ParameterizedThreadStart(TransferData));
+            thread.Start(tpp);
+        }
+    }
+    public class ThreadProcParams
+    {
+        public int port;
+        public string host;
+        public ThreadProcParams(int port, string host)
+        {
+            this.port = port;
+            this.host = host;
+        }
+    }
+}
